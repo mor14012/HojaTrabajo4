5ee287b18bce0c38cd46e22dd42917ab0f30fce7
octajul77 <gon14096@uvg.edu.gt>
2015-08-16 11:52:55 -0500

    Update doubly.java

--- Beta0.3/doubly.java
+++ Beta0.3/doubly.java
@@ -1,9 +1,33 @@
-public class doubly<E> extends simple<E>{
+public class doubly<E> extends linkedlist<E>{
+	
+	protected doblenodo<E> head;
+	protected doblenodo<E> tail;
+	
 	
 	public void doubly(){
+	head=null;
+	tail=null;
+	}
+	
+	public  void addFirst (E value){
+		head= new doblenodo<E>(value,head,null);
+		size++;
+			if (tail ==null){
+				tail=head;
+			}
 	
 	}
 	
+	public E removeFirst(){
+		if(size!=0){
+			doblenodo<E> temp=head;
+			head=head.getNext();
+			size--;
+		return temp.getValue();
+		}else{
+			return null;
+			}
+	}
 	
 	
 }
