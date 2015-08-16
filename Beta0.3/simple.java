2af391b788d25f7fd8fc706f3937a2071a36de87
octajul77 <gon14096@uvg.edu.gt>
2015-08-15 23:45:05 -0500

    Create simple.java

---
+++ Beta0.3/simple.java
@@ -0,0 +1,34 @@
+
+public class simple<E> extends linkedlist<E>{
+	protected nodo <E> head;
+
+	public void simple(){
+		head = null;
+	}
+	
+	public void addFirst(E value){
+	
+		head= new nodo<E>(value, head);
+		size++;
+		
+	
+		}
+		
+		
+	
+		
+	public E removeFirst(){ 
+		if(size!=0){
+			nodo<E> temp= head;
+			head=head.getNext();
+			size--;
+			return temp.getValue();
+			}
+		else{
+			return null;
+		}
+	
+	}
+	
+	
+}
