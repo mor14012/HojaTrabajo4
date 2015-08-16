12e73620e034e31798b3723d89aa0b5d6f4adb00
octajul77 <gon14096@uvg.edu.gt>
2015-08-15 23:50:13 -0500

    Create factory.java

---
+++ Beta0.3/factory.java
@@ -0,0 +1,15 @@
+public class factory{
+	public stack getStack(String object){
+		if(object.equals("Vector"))
+			return new vector();
+		if(object.equals("ArrayList"))
+			return new arraylist();
+		if(object.equals("Simple"))
+			return new simple();
+		if(object.equals("Doubly"))
+			return new doubly();
+		if(object.equals("Circular"))
+			return new circular();
+		return null;
+	}
+}
